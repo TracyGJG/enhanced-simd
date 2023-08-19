599bb976a8568855253d15402d5d64135f86263b
Tracy-Gregory Gilmore <tracyg.gilmore@gmail.com>
2023-08-19 19:06:59 +0100

    Prettified again

--- src/esimd.js
+++ src/esimd.js
@@ -1,111 +1,119 @@
 const ExecutionMode = {
-    NO_CACHE: 0,
-    CACHED: 1,
-    MATRIX: 2,
+  NO_CACHE: 0,
+  CACHED: 1,
+  MATRIX: 2,
+};
+
+function esimd(instruction, executionMode = ExecutionMode.NO_CACHE) {
+  let caches = [...Array(instruction.length)].fill([]);
+
+  return async function (...dataSources) {
+    const alignmentError = checkAlignment(instruction.length, dataSources);
+    if (alignmentError) {
+      throw alignmentError;
+    }
+    const capacityError = checkCapacity(executionMode, dataSources);
+    if (capacityError) {
+      throw capacityError;
+    }
+
+    let dataFeeds;
+    if (executionMode === ExecutionMode.CACHED) {
+      dataFeeds = caches.map((cache, index) => [
+        ...cache,
+        ...structuredClone(dataSources[index]),
+      ]);
+      caches = dataFeeds;
+    } else {
+      dataFeeds = structuredClone(dataSources);
+    }
+
+    const executions = (
+      executionMode === ExecutionMode.MATRIX ? permute : transform
+    )(instruction, dataFeeds);
+
+    const results = await Promise.allSettled(executions);
+
+    const valiationError = validateResults(results);
+    if (valiationError) {
+      throw valiationError;
+    }
+    return results.map((result) => result.value);
   };
-  
-function esimd(
-    instruction, 
-    executionMode = ExecutionMode.NO_CACHE
-) {
-    let caches = [...Array(instruction.length)].fill([]);
-
-    return async function (...dataSources) {
-        const alignmentError = checkAlignment(instruction.length, dataSources);
-        if (alignmentError) {
-            throw alignmentError;
-        }
-        const capacityError = checkCapacity(executionMode, dataSources);
-        if (capacityError) {
-            throw capacityError;
-        }
-
-        let dataFeeds;
-        if (executionMode === ExecutionMode.CACHED) {
-            dataFeeds = caches.map((cache, index) => [...cache, ...structuredClone(dataSources[index])]);
-            caches = dataFeeds;    
-        }
-        else {
-            dataFeeds = structuredClone(dataSources);
-        }
-
-        const executions = (executionMode === ExecutionMode.MATRIX
-            ? permute : transform)(instruction, dataFeeds);
-
-        const results = await Promise.allSettled(executions);
-
-        const valiationError = validateResults(results);
-        if (valiationError) {
-            throw valiationError;
-        }
-        return results.map(result => result.value);      
-    };
 }
 
 function permute(fn, axies) {
-    return _permute().flat(axies.length - 1);
+  return _permute().flat(axies.length - 1);
 
-    function _permute(...buffers) {
-        const buffersLength = buffers.length;
-        return ((buffersLength === axies.length)
-            ? executeInstruction(fn)(buffers)
-            : axies[buffersLength].map(axis => _permute(...buffers, axis)));
-    }
+  function _permute(...buffers) {
+    const buffersLength = buffers.length;
+    return buffersLength === axies.length
+      ? executeInstruction(fn)(buffers)
+      : axies[buffersLength].map((axis) => _permute(...buffers, axis));
+  }
 }
 
 function transform(fn, axies) {
-    return _transpose().map(
-        executeInstruction(fn)
-    );
+  return _transpose().map(executeInstruction(fn));
 
-    function _transpose() {
-        const minAxisLength = Math.min(...(axies.map(axis => axis.length)));
-        const buffers = axies.map(axis => axis.splice(0, minAxisLength));
-        const swapRowColumn = (_, row) => row.map((__, i) => [...(_[i] || []), row[i]]);
-        return buffers.reduce( swapRowColumn, []);
-    }
+  function _transpose() {
+    const minAxisLength = Math.min(...axies.map((axis) => axis.length));
+    const buffers = axies.map((axis) => axis.splice(0, minAxisLength));
+    const swapRowColumn = (_, row) =>
+      row.map((__, i) => [...(_[i] || []), row[i]]);
+    return buffers.reduce(swapRowColumn, []);
+  }
 }
 
 function executeInstruction(fnInstruction) {
-    return (buffers) => new Promise((resolve, reject) => {
-        try {
-            resolve(fnInstruction(...buffers));
-        } catch (error) {
-            reject(error);
-        }
+  return (buffers) =>
+    new Promise((resolve, reject) => {
+      try {
+        resolve(fnInstruction(...buffers));
+      } catch (error) {
+        reject(error);
+      }
     });
 }
 
 function checkAlignment(numParams, dataFeeds) {
-    if (dataFeeds.length !== numParams) {
-        return Error('esimd: Error - Mismatch between the number of data feeds and the parameters of the instruction');
-    }
+  if (dataFeeds.length !== numParams) {
+    return Error(
+      'esimd: Error - Mismatch between the number of data feeds and the parameters of the instruction'
+    );
+  }
 }
 
 function checkCapacity(mode, dataFeeds) {
-    const MAX_NUMBER_OF_FEEDS = 10;
-    const MAX_SIZE_OF_FEED = 10;
-    const MAX_SIZE_OF_MATRIX = 10_000;
-    
-    if (mode === ExecutionMode.MATRIX) {
-        if (dataFeeds.length > MAX_NUMBER_OF_FEEDS) {
-            return Error(`esimd: Error - The number of data feeds exceeds the maximum of ${MAX_NUMBER_OF_FEEDS}.`);
-        }
-        const feedSizes = dataFeeds.map(dataFeed => dataFeed.length);
-        if (Math.max(...feedSizes) > MAX_SIZE_OF_FEED) {
-            return Error(`esimd: Error - On or more of the data feeds exceeds the maximum size of ${MAX_SIZE_OF_FEED}.`);
-        }
-        const matrixSize = feedSizes.reduce((total, size) => total * size);
-        if (matrixSize > MAX_SIZE_OF_MATRIX) {
-            return Error(`esimd: Error - The calculated size of the output matrix exceeds the maximum size of ${MAX_SIZE_OF_MATRIX}.`);
-        }
+  const MAX_NUMBER_OF_FEEDS = 10;
+  const MAX_SIZE_OF_FEED = 10;
+  const MAX_SIZE_OF_MATRIX = 10_000;
+
+  if (mode === ExecutionMode.MATRIX) {
+    if (dataFeeds.length > MAX_NUMBER_OF_FEEDS) {
+      return Error(
+        `esimd: Error - The number of data feeds exceeds the maximum of ${MAX_NUMBER_OF_FEEDS}.`
+      );
     }
+    const feedSizes = dataFeeds.map((dataFeed) => dataFeed.length);
+    if (Math.max(...feedSizes) > MAX_SIZE_OF_FEED) {
+      return Error(
+        `esimd: Error - On or more of the data feeds exceeds the maximum size of ${MAX_SIZE_OF_FEED}.`
+      );
+    }
+    const matrixSize = feedSizes.reduce((total, size) => total * size);
+    if (matrixSize > MAX_SIZE_OF_MATRIX) {
+      return Error(
+        `esimd: Error - The calculated size of the output matrix exceeds the maximum size of ${MAX_SIZE_OF_MATRIX}.`
+      );
+    }
+  }
 }
 
 function validateResults(executionResults) {
-    if (executionResults.some(result => result.status !== 'fulfilled')) {
-        return Error('esimd: Error - Instruction execution failed');
-    }
+  if (executionResults.some((result) => result.status !== 'fulfilled')) {
+    return Error('esimd: Error - Instruction execution failed');
+  }
 }
 
 exports.esimd = esimd;
--- src/esimd.test.js
+++ src/esimd.test.js
@@ -1,11 +1,11 @@
-const Esimd = require("./esimd.js");
+const Esimd = require('./esimd.js');
 
 const testData = {
   empty: [[], [], []],
   populated: [
-    ["A", "B"],
-    ["C", "D", "E"],
-    ["F", "0", "1", "2"],
+    ['A', 'B'],
+    ['C', 'D', 'E'],
+    ['F', '0', '1', '2'],
   ],
 };
 
@@ -14,185 +14,185 @@ function process(a, b, c) {
   if (result.trim().length === 3) {
     return result;
   }
-  throw Error("Process fault");
+  throw Error('Process fault');
 }
 
-describe("Esimd", () => {
-  describe("Properties", () => {
-    test("has an ExecutionMode enumeration", () => {
+describe('Esimd', () => {
+  describe('Properties', () => {
+    test('has an ExecutionMode enumeration', () => {
       expect(Esimd.ExecutionMode).toBeDefined();
-      expect(typeof Esimd.ExecutionMode).toBe("object");
+      expect(typeof Esimd.ExecutionMode).toBe('object');
       expect(Esimd.ExecutionMode.NO_CACHE).toBe(0);
       expect(Esimd.ExecutionMode.CACHED).toBe(1);
       expect(Esimd.ExecutionMode.MATRIX).toBe(2);
     });
 
-    test("has an esimd method", () => {
+    test('has an esimd method', () => {
       expect(Esimd.esimd).toBeDefined();
-      expect(typeof Esimd.esimd).toBe("function");
+      expect(typeof Esimd.esimd).toBe('function');
     });
   });
 
-  describe("No Cache operations", () => {
+  describe('No Cache operations', () => {
     let esimdInstruction;
 
     beforeEach(() => {
       esimdInstruction = Esimd.esimd(process);
     });
 
-    test("with three empty arrays", async () => {
+    test('with three empty arrays', async () => {
       const result = await esimdInstruction(...testData.empty);
       expect(result.length).toBe(0);
     });
 
-    test("with three populated arrays", async () => {
+    test('with three populated arrays', async () => {
       const result = await esimdInstruction(...testData.populated);
       expect(result.length).toBe(2);
-      expect(result[0]).toBe("ACF");
-      expect(result[1]).toBe("BD0");
+      expect(result[0]).toBe('ACF');
+      expect(result[1]).toBe('BD0');
     });
 
-    test("with three populated arrays (without caching)", async () => {
+    test('with three populated arrays (without caching)', async () => {
       let result = await esimdInstruction(...testData.populated);
       expect(result.length).toBe(2);
-      expect(result[0]).toBe("ACF");
-      expect(result[1]).toBe("BD0");
+      expect(result[0]).toBe('ACF');
+      expect(result[1]).toBe('BD0');
 
       const reversedTestData = structuredClone(testData.populated).reverse();
       result = await esimdInstruction(...reversedTestData);
       expect(result.length).toBe(2);
-      expect(result[0]).toBe("FCA");
-      expect(result[1]).toBe("0DB");
+      expect(result[0]).toBe('FCA');
+      expect(result[1]).toBe('0DB');
     });
 
-    test("with a mismatched input", async () => {
+    test('with a mismatched input', async () => {
       const exceptionTest = () => esimdInstruction([], []);
       expect(exceptionTest).rejects.toThrow(
-        "esimd: Error - Mismatch between the number of data feeds and the parameters of the instruction"
+        'esimd: Error - Mismatch between the number of data feeds and the parameters of the instruction'
       );
     });
 
-    test("with a failed execution", async () => {
+    test('with a failed execution', async () => {
       const exceptionTest = () =>
-        esimdInstruction(["a", "b"], ["c", "d"], ["e", " "]);
+        esimdInstruction(['a', 'b'], ['c', 'd'], ['e', ' ']);
       expect(exceptionTest).rejects.toThrow(
-        "esimd: Error - Instruction execution failed"
+        'esimd: Error - Instruction execution failed'
       );
     });
   });
 
-  describe("Cached operations", () => {
+  describe('Cached operations', () => {
     let esimdInstruction;
 
     beforeEach(() => {
       esimdInstruction = Esimd.esimd(process, Esimd.ExecutionMode.CACHED);
     });
 
-    test("with three empty arrays", async () => {
+    test('with three empty arrays', async () => {
       const result = await esimdInstruction(...testData.empty);
       expect(result.length).toBe(0);
     });
 
-    test("with three populated arrays", async () => {
+    test('with three populated arrays', async () => {
       const result = await esimdInstruction(...testData.populated);
       expect(result.length).toBe(2);
-      expect(result[0]).toBe("ACF");
-      expect(result[1]).toBe("BD0");
+      expect(result[0]).toBe('ACF');
+      expect(result[1]).toBe('BD0');
     });
 
-    test("with three populated arrays (without caching)", async () => {
+    test('with three populated arrays (without caching)', async () => {
       let result = await esimdInstruction(...testData.populated);
       expect(result.length).toBe(2);
-      expect(result[0]).toBe("ACF");
-      expect(result[1]).toBe("BD0");
+      expect(result[0]).toBe('ACF');
+      expect(result[1]).toBe('BD0');
 
       const reversedTestData = structuredClone(testData.populated).reverse();
       result = await esimdInstruction(...reversedTestData);
       expect(result.length).toBe(4);
-      expect(result[0]).toBe("FE1");
-      expect(result[1]).toBe("0C2");
-      expect(result[2]).toBe("1DA");
-      expect(result[3]).toBe("2EB");
+      expect(result[0]).toBe('FE1');
+      expect(result[1]).toBe('0C2');
+      expect(result[2]).toBe('1DA');
+      expect(result[3]).toBe('2EB');
     });
   });
 
-  describe("Matrix operations", () => {
+  describe('Matrix operations', () => {
     let esimdInstruction;
 
     beforeEach(() => {
       esimdInstruction = Esimd.esimd(process, Esimd.ExecutionMode.MATRIX);
     });
 
-    test("with three empty arrays", async () => {
+    test('with three empty arrays', async () => {
       const result = await esimdInstruction(...testData.empty);
       expect(result.length).toBe(0);
     });
 
-    test("with three populated arrays", async () => {
+    test('with three populated arrays', async () => {
       const result = await esimdInstruction(...testData.populated);
       expect(result.length).toBe(24);
-      expect(result[0]).toBe("ACF");
-      expect(result[1]).toBe("AC0");
-      expect(result[23]).toBe("BE2");
+      expect(result[0]).toBe('ACF');
+      expect(result[1]).toBe('AC0');
+      expect(result[23]).toBe('BE2');
     });
 
-    test("reports an exception when too many data feeds are supplied for a matrix", () => {
-      const instruction = (_a, _b, _c, _d, _e, _f, _g, _h, _i, _j, _k) => "";
+    test('reports an exception when too many data feeds are supplied for a matrix', () => {
+      const instruction = (_a, _b, _c, _d, _e, _f, _g, _h, _i, _j, _k) => '';
       const esimdInstruction2 = Esimd.esimd(
         instruction,
         Esimd.ExecutionMode.MATRIX
       );
       const args = [
-        ["A", "B"],
-        ["C", "D"],
-        ["E", "F"],
-        ["G", "H"],
-        ["I", "J"],
-        ["K", "L"],
-        ["M", "N"],
-        ["O", "P"],
-        ["Q", "R"],
-        ["S", "T"],
-        ["U", "V"],
+        ['A', 'B'],
+        ['C', 'D'],
+        ['E', 'F'],
+        ['G', 'H'],
+        ['I', 'J'],
+        ['K', 'L'],
+        ['M', 'N'],
+        ['O', 'P'],
+        ['Q', 'R'],
+        ['S', 'T'],
+        ['U', 'V'],
       ];
 
       const exceptionTest = async () => await esimdInstruction2(...args);
 
       expect(exceptionTest).rejects.toThrow(
-        "esimd: Error - The number of data feeds exceeds the maximum of 10."
+        'esimd: Error - The number of data feeds exceeds the maximum of 10.'
       );
     });
 
-    test("reports an exception when a data feed is too large", () => {
+    test('reports an exception when a data feed is too large', () => {
       expect(async () => {
         await esimdInstruction(
-          ["A", "B"],
-          ["C", "D"],
-          ["E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O"]
+          ['A', 'B'],
+          ['C', 'D'],
+          ['E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O']
         );
       }).rejects.toThrow(
-        "esimd: Error - On or more of the data feeds exceeds the maximum size of 10."
+        'esimd: Error - On or more of the data feeds exceeds the maximum size of 10.'
       );
     });
 
-    test("reports an exception when the calculated size of the resultant matrix is too large", () => {
-      const instruction = (_a, _b, _c, _d, _e) => "";
+    test('reports an exception when the calculated size of the resultant matrix is too large', () => {
+      const instruction = (_a, _b, _c, _d, _e) => '';
       const esimdInstruction2 = Esimd.esimd(
         instruction,
         Esimd.ExecutionMode.MATRIX
       );
       const args = [
-        ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"],
-        ["K", "L", "M", "N", "O", "P", "Q", "R", "S", "T"],
-        ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"],
-        ["K", "L", "M", "N", "O", "P", "Q", "R", "S", "T"],
-        ["U", "V"],
+        ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'],
+        ['K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T'],
+        ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'],
+        ['K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T'],
+        ['U', 'V'],
       ];
 
       const exceptionTest = async () => await esimdInstruction2(...args);
 
       expect(exceptionTest).rejects.toThrow(
-        "esimd: Error - The calculated size of the output matrix exceeds the maximum size of 10000."
+        'esimd: Error - The calculated size of the output matrix exceeds the maximum size of 10000.'
       );
     });
   });
